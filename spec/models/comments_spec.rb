d1bc743bdab6eefbdd5effcf8b52710208f10054
Emmanuel Okwara <emma4real37@gmail.com>
2019-09-29 10:14:17 +0100

    feat-[168821698]: Create comment
    - A user should be able to create a comment
    - Initial comment can only be created by admin or agent
    
    [Finishes #168821698]

--- spec/models/comments_spec.rb
+++ spec/models/comments_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe Comment, type: :model do
   describe "Validations" do
     describe "#text" do
       it { should validate_presence_of(:text) }
-      it { should validate_length_of(:text).is_at_least(3).is_at_most(50) }
+      it {
+        should validate_length_of(:text).
+          is_at_least(3).
+          is_at_most(100)
+      }
     end
   end
 end
